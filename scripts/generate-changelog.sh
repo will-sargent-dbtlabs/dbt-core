671aff06375553376dd96107cfed2c59070c9ae0
Emily Rockman <emily.rockman@dbtlabs.com>
2022-02-18 15:52:42 -0600

    update to reflect auto footer

--- scripts/generate-changelog.sh
+++ scripts/generate-changelog.sh
@@ -6,21 +6,18 @@ read -p "Final release? (Y/N): " final
 if [ "$final" == "N" ] || [ "$final" == "n" ]; then
     read -p "Enter pre-release (rc, etc): " prerelease
     full_version="$version-$prerelease"
-#     changie batch $full_version --keep
-#     if [ ! -d /.changes/$version ]; then
-#         mkdir ./.changes/$version
-#     fi
+    changie batch $full_version --keep
+    if [ ! -d /.changes/$version ]; then
+        mkdir ./.changes/$version
+    fi
 
-#     # TODO: not sure why there would not be any changelog files, but check in case
-#     mv ./.changes/unreleased/* ./.changes/$version/
+    mv ./.changes/unreleased/* ./.changes/$version/
 
 else
     full_version=$version
-    # TODO: remove all prerelease lines from current changelog
+    rm -rf ./.changes/$version*
     mv ./.changes/$version/* ./.changes/unreleased/
     changie batch $full_version
 fi
-
-python ./scripts/add-contributors.py $full_version
 
 changie merge
