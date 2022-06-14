ea35fd14546f501c2a0089efa89ae54a8dcc41a8
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-06-14 16:10:22 -0500

    completing init default versions of all macros being called for look over and collaboration

--- core/dbt/include/global_project/macros/adapters/apply_grants.sql
+++ core/dbt/include/global_project/macros/adapters/apply_grants.sql
@@ -1,5 +1,5 @@
 {% macro get_show_grant_sql(relation) %}
-{{ return(adapter.dispatch('get_show_grant_sql', 'dbt')(relation)) }}
+{{ return(adapter.dispatch("get_show_grant_sql", "dbt")(relation)) }}
 {% endmacro %}
 
 {% macro default__get_show_grant_sql(relation) %}
@@ -11,19 +11,27 @@ show grants on {{ relation.type }} {{ relation }}
 {% endmacro %}
 
 {% macro default__get_grant_sql(relation, grant_config) %}
-grant {{ privalage }} on {{ relation.type }} {{ relation }} to {{ recipients }}
+grant "select" on {{ relation.type }} {{ relation }} to {{ grant_config["select"].join(", ") }}
 {% endmacro %}
 
 {% macro get_revoke_sql(relation, grant_config) %}
-{{ return(adapter.dispatch('get_revoke_sql', 'dbt')(relation, grant_config)) }}
+{{ return(adapter.dispatch("get_revoke_sql", "dbt")(relation, grant_config)) }}
 {% endmacro %}
 
 {% macro default__get_revoke_sql(relation, grant_config) %}
+revoke "select" on {{ relation }} from {{ grant_config["select"].join(", ") }}
 {% endmacro %}
 
 {% macro apply_grants(relation, grant_config, should_revoke) %}
-{{ return(adapter.dispatch('apply_grant', 'dbt')(relation, grant_config, should_revoke)) }}
+{{ return(adapter.dispatch("apply_grants", "dbt")(relation, grant_config, should_revoke)) }}
 {% endmacro %}
 
-{% macro default__apply_grants(revoke, relation, grant_config) %}
+{% macro default__apply_grants(relation, grant_config, should_revoke) %}
+{% if "select" not in grant_config %}
+get_show_grant_sql(relation)
+    {% if should_revoke %}
+    get_revoke_sql(relation, grant_config)
+    {% endif%}
+get_grant_sql(relation, grant_config)
+{% endif%}
 {% endmacro %}
