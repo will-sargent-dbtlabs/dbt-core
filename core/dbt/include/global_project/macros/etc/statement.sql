6edb9fd4d7e511d6ea5306b00befec5cd1ba1166
Ian Knox <ian.knox@dbtlabs.com>
2022-06-23 15:54:45 -0500

    cleaned up incremental logic

--- core/dbt/include/global_project/macros/etc/statement.sql
+++ core/dbt/include/global_project/macros/etc/statement.sql
@@ -1,13 +1,26 @@
-{% macro statement(name=None, fetch_result=False, auto_begin=True) -%}
+{#-- 
+TODO:  Only certian adapters need to support python models so this should probably be 
+moved to an override I.E. `spark__statement`  but for some reason it wasn't picking up that change
+when implemented in dbt-spark/dbt/include/spark/macros/adapters.sql
+--#}
+
+{%- macro statement(name=None, fetch_result=False, auto_begin=True, language='sql') -%}
   {%- if execute: -%}
-    {%- set sql = caller() -%}
+    {%- set model_code = caller() -%}
 
     {%- if name == 'main' -%}
-      {{ log('Writing runtime SQL for node "{}"'.format(model['unique_id'])) }}
-      {{ write(sql) }}
+      {{ log('Writing runtime {} for node "{}"'.format(language, model['unique_id'])) }}
+      {{ write(model_code) }}
     {%- endif -%}
 
-    {%- set res, table = adapter.execute(sql, auto_begin=auto_begin, fetch=fetch_result) -%}
+    {%- if language == 'sql'-%}
+      {%- set res, table = adapter.execute(model_code, auto_begin=auto_begin, fetch=fetch_result) -%}
+    {%- elif language == 'python' -%}
+      {%- set res = adapter.submit_python_job(schema, model['alias'], model_code) -%}
+      {#-- TODO: What should table be for python models?--#}
+      {%- set table = None -%}
+    {%- endif -%}
+
     {%- if name is not none -%}
       {{ store_result(name, response=res, agate_table=table) }}
     {%- endif -%}
