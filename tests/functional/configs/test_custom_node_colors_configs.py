1c1c6a02e399fc6b45965374c2ac156213c62d74
Sung Won Chung <sungwonchung3@gmail.com>
2022-07-27 14:25:52 -0500

    first draft

--- tests/functional/configs/test_custom_node_colors_configs.py
+++ tests/functional/configs/test_custom_node_colors_configs.py
@@ -1,12 +1,14 @@
 import pytest
 
 from tests.functional.configs.fixtures import BaseConfigProject
+from dbt.tests.util import run_dbt, get_manifest
+
 
 models__custom_node_colors__model_sql = """
 {{
     config(
         materialized='view',
-        node_color='#c0c0c0'
+        node_color='#000000'
     )
 }}
 
@@ -14,7 +16,7 @@ select 1 as id
 
 """
 
-models_custom_node_colors__schema_yml = """
+models__custom_node_colors__schema_yml = """
 version: 2
 
 models:
@@ -22,6 +24,14 @@ models:
     description: "This is a model description"
     config:
         node_color: '#c0c0c0'
+"""
+
+dbt_project_yml = """
+models:
+  test:
+    my_model:
+      +docs:
+        node_color: "#000000"
 """
 
 
@@ -39,3 +49,37 @@ class TestCustomNodeColorConfigs(BaseConfigProject):
         project,
     ):
         pass
+
+
+# python3 -m pytest tests/functional/configs/test_custom_node_colors_configs.py
+# TODO: node_color at model level creates a docs object underneath config within the node manifest, and a root level docs object in the node manifest as well
+class TestGrantConfigs:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {"my_model.sql": models__custom_node_colors__model_sql}
+
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return dbt_project_yml
+
+    def test_model_node_color_config(self, project):
+        # compile the project to generate the manifest
+        run_dbt(["compile"])
+
+        # get the manifest
+        manifest = get_manifest(project.project_root)
+
+        # get the node config for the model within the manifest
+        model_id = "model.test.my_model"
+        model_node_config = manifest.nodes[model_id]
+        node_color_actual = model_node_config.config.docs["node_color"]
+
+        node_color_expected = "#c0c0c0"
+
+        # assert the node_color is as expected
+        assert node_color_actual == node_color_expected
+
+
+# TODO: node_color in schema.yml overrides dbt_project.yml creates a docs object underneath config within the node manifest, and a root level docs object in the node manifest as well
+
+# TODO: node_color in a subfolder overrides global node_color in dbt_project.yml creates a docs object underneath config within the node manifest, and a root level docs object in the node manifest as well
