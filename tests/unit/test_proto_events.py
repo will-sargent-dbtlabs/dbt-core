8a892dbec65a2d336b49606bfe4ceddf1257edde
Gerda Shank <gerda@dbtlabs.com>
2022-08-11 11:53:23 -0400

    exclude _pb2.py in pre-commit

--- tests/unit/test_proto_events.py
+++ tests/unit/test_proto_events.py
@@ -7,6 +7,7 @@ from google.protobuf import json_format
 
 def test_events():
 
+    # A001 event
     event = MainReportVersion(v=str(installed))
     json_event = event.to_json()
 
@@ -18,3 +19,18 @@ def test_events():
 
     event_dict = json_format.MessageToDict(event1)
     assert event_dict == {"code": "A001", "v": str(installed)}
+
+    # A002 event
+    # Note: the "args" dict is defined as <string, string> so the values
+    # must be strings.
+    event = MainReportArgs(args={"one": "1", "two": "2"})
+    json_event = event.to_json()
+    print(f"--- A002 json_event: {json_event}")
+
+    event2 = A002()
+    assert event2
+
+    event2 = json_format.Parse(json_event, event2)
+
+    event_dict = json_format.MessageToDict(event2)
+    assert event_dict == {"code": "A002", "args": {"one": "1", "two": "2"}}
