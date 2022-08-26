4e90f87251847f529c2c706c82793e39e7c3ba37
Callum McCann <cmccann51@gmail.com>
2022-08-26 12:10:48 -0500

    file changes

--- core/dbt/contracts/graph/parsed.py
+++ core/dbt/contracts/graph/parsed.py
@@ -812,6 +812,7 @@ class ParsedMetric(UnparsedBaseNode, HasUniqueID, HasFqn):
     filters: List[MetricFilter]
     time_grains: List[str]
     dimensions: List[str]
+    lookback: Optional[str]
     model: Optional[str] = None
     model_unique_id: Optional[str] = None
     resource_type: NodeType = NodeType.Metric
@@ -833,6 +834,9 @@ class ParsedMetric(UnparsedBaseNode, HasUniqueID, HasFqn):
 
     def same_model(self, old: "ParsedMetric") -> bool:
         return self.model == old.model
+
+    def same_lookback(self, old: "ParsedMetric") -> bool:
+        return self.lookback == old.lookback
 
     def same_dimensions(self, old: "ParsedMetric") -> bool:
         return self.dimensions == old.dimensions
@@ -866,6 +870,7 @@ class ParsedMetric(UnparsedBaseNode, HasUniqueID, HasFqn):
 
         return (
             self.same_model(old)
+            and self.same_lookback(old)
             and self.same_dimensions(old)
             and self.same_filters(old)
             and self.same_description(old)
--- core/dbt/parser/schemas.py
+++ core/dbt/parser/schemas.py
@@ -1050,6 +1050,7 @@ class MetricParser(YamlReader):
             sql=str(unparsed.sql),
             timestamp=unparsed.timestamp,
             dimensions=unparsed.dimensions,
+            lookback=unparsed.lookback,
             time_grains=unparsed.time_grains,
             filters=unparsed.filters,
             meta=unparsed.meta,
