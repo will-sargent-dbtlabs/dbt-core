656cb394d891bc9225b6c1ec717e6f8417a4ddef
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2022-08-30 11:29:08 -0500

    renamed expected_sql to include the test_test_ephemeral style name, organized how models are imported into test classes

--- tests/functional/ephemeral/test_ephemeral.py
+++ tests/functional/ephemeral/test_ephemeral.py
@@ -54,7 +54,7 @@ select * from {{ ref('base') }}
 
 ephemeral_errors__dependent_sql = """
 -- base copy is an error
-select * from {{ref('base_copy_errors')}} where gender = 'Male'
+select * from {{ref('base_copy')}} where gender = 'Male'
 
 """
 
@@ -70,7 +70,7 @@ ephemeral_errors__base__base_copy_sql = """
 
 {{ adapter.invalid_method() }}
 
-select * from {{ ref('base_errors') }}
+select * from {{ ref('base') }}
 
 """
 
@@ -116,33 +116,25 @@ from source_data
 """
 
 
-@pytest.fixture(scope="class")
-def models():
-    return {
-        "dependent.sql": models__dependent_sql,
-        "double_dependent.sql": models__double_dependent_sql,
-        "super_dependent.sql": models__super_dependent_sql,
-        "ephemeral_level_two.sql": models_n__ephemeral_level_two_sql,
-        "root_view.sql": models_n__root_view_sql,
-        "ephemeral.sql": models_n__ephemeral_sql,
-        "source_table.sql": models_n__source_table_sql,
-        "dependent_errors.sql": ephemeral_errors__dependent_sql,
-        "base": {
-            "female_only.sql": models__base__female_only_sql,
-            "base.sql": models__base__base_sql,
-            "base_copy.sql": models__base__base_copy_sql,
-            "base_errors.sql": ephemeral_errors__base__base_sql,
-            "base_copy_errors.sql": ephemeral_errors__base__base_copy_sql,
-        },
-    }
-
-
 @pytest.fixture(scope="class", autouse=True)
 def setUp(project):
     project.run_sql_file(project.test_data_dir / Path("seed.sql"))
 
 
 class TestEphemeralMulti:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "dependent.sql": models__dependent_sql,
+            "double_dependent.sql": models__double_dependent_sql,
+            "super_dependent.sql": models__super_dependent_sql,
+            "base": {
+                "female_only.sql": models__base__female_only_sql,
+                "base.sql": models__base__base_sql,
+                "base_copy.sql": models__base__base_copy_sql,
+            },
+        }
+
     def test_ephemeral_multi(self, project):
         results = run_dbt(["run"])
         assert len(results) == 3
@@ -156,9 +148,9 @@ class TestEphemeralMulti:
 
         sql_file = re.sub(r"\d+", "", sql_file)
         expected_sql = (
-            'create view "dbt"."test_ephemeral"."double_dependent__dbt_tmp" as ('
+            'create view "dbt"."test_test_ephemeral"."double_dependent__dbt_tmp" as ('
             "with __dbt__cte__base as ("
-            "select * from test_ephemeral.seed"
+            "select * from test_test_ephemeral.seed"
             "),  __dbt__cte__base_copy as ("
             "select * from __dbt__cte__base"
             ")-- base_copy just pulls from base. Make sure the listed"
@@ -174,10 +166,17 @@ class TestEphemeralMulti:
 
 
 class TestEphemeralNested:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "ephemeral_level_two.sql": models_n__ephemeral_level_two_sql,
+            "root_view.sql": models_n__root_view_sql,
+            "ephemeral.sql": models_n__ephemeral_sql,
+            "source_table.sql": models_n__source_table_sql,
+        }
+
     def test_ephemeral_nested(self, project):
-        results = run_dbt(
-            ["run", "--models", "ephemeral_level_two", "root_view", "ephemeral", "source_table"]
-        )
+        results = run_dbt(["run"])
         assert len(results) == 2
         assert os.path.exists("./target/run/test/models/root_view.sql")
         with open("./target/run/test/models/root_view.sql", "r") as fp:
@@ -185,9 +184,9 @@ class TestEphemeralNested:
 
         sql_file = re.sub(r"\d+", "", sql_file)
         expected_sql = (
-            'create view "dbt"."test_ephemeral"."root_view__dbt_tmp" as ('
+            'create view "dbt"."test_test_ephemeral"."root_view__dbt_tmp" as ('
             "with __dbt__cte__ephemeral_level_two as ("
-            'select * from "dbt"."test_ephemeral"."source_table"'
+            'select * from "dbt"."test_test_ephemeral"."source_table"'
             "),  __dbt__cte__ephemeral as ("
             "select * from __dbt__cte__ephemeral_level_two"
             ")select * from __dbt__cte__ephemeral"
@@ -200,8 +199,18 @@ class TestEphemeralNested:
 
 
 class TestEphemeralErrorHandling:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "dependent.sql": ephemeral_errors__dependent_sql,
+            "base": {
+                "base.sql": ephemeral_errors__base__base_sql,
+                "base_copy.sql": ephemeral_errors__base__base_copy_sql,
+            },
+        }
+
     def test_ephemeral_error_handling(self, project):
-        results = run_dbt(["run", "--models", "dependent_errors"], expect_pass=False)
+        results = run_dbt(["run"], expect_pass=False)
         assert len(results) == 1
         assert results[0].status == "skipped"
         assert "Compilation Error" in results[0].message
